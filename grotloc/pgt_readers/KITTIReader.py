692a1c3612562caa637d57f550cbff5bc603fe6a
Nicolas Soncini <soncininicolas@gmail.com>
2022-03-28 11:05:10 -0300

    Improve on KITTI PGT reader

--- grotloc/pgt_readers/KITTIReader.py
+++ grotloc/pgt_readers/KITTIReader.py
@@ -38,3 +38,25 @@ class KITTIReader():
     def read_pgt(self, input_file):
         poses = pd.read_csv(input_file, sep=' ', names=self.KITTI_COL_NAMES)
         return poses
+
+
+class KITTITimestampedReader():
+    """
+    Pose Ground-Truth reader that conforms to the KITTI Odometry format
+    http://www.cvlibs.net/datasets/kitti/eval_odometry.php
+    with an additional initial timestamp column
+    """
+    
+    # KITTI represents odometry in 4x4 homogeneous pose matrices
+    # and only writes the 3x3 rotation and 3x1 translation.
+    # The missing row would be [0, 0, 0, 1]
+    KITTI_COL_NAMES = [
+        'time',
+        'r11', 'r12', 'r13', 'tx',
+        'r21', 'r22', 'r23', 'ty',
+        'r31', 'r32', 'r33', 'tz'
+    ]
+
+    def read_pgt(self, input_file):
+        poses = pd.read_csv(input_file, sep=' ', names=self.KITTI_COL_NAMES)
+        return poses
