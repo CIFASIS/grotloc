bd988fe9d504a53d2124e46212ecc52962d46b33
Nicolas Soncini <soncininicolas@gmail.com>
2022-03-29 18:36:08 -0300

    Add 2d scatterplot widget to display tools. Fix 3d scatterplot linting.

--- grotloc/utils/pysimplegui_utils/loop_map_widget.py
+++ grotloc/utils/pysimplegui_utils/loop_map_widget.py
@@ -20,6 +20,7 @@ __version__ = "0.0.1"
 from grotloc.utils.pysimplegui_utils.gui_widget import GUIWidget
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.collections import LineCollection
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 import PySimpleGUI as sg
 import matplotlib.colors as mcolors
@@ -79,8 +80,10 @@ class ScatterMap3D(GUIWidget):
         return window
 
     def scatter_figure(self):
+        """ Helper function to plot with PyPlot """
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
+
         ax.set_xlabel(self.columns[0])
         xvals = self.data_points[self.columns[0]]
 
@@ -92,7 +95,7 @@ class ScatterMap3D(GUIWidget):
 
         # given the pairs of candidates as (i1,i2), get the pairs of loops as
         # ((x1,y1,z1),(x2,y2,z2))
-        candidate_pairs_3d = list(map(
+        candidate_pairs = list(map(
             lambda i: [
                     (xvals.iloc[i[0]], yvals.iloc[i[0]], zvals.iloc[i[0]]),
                     (xvals.iloc[i[1]], yvals.iloc[i[1]], zvals.iloc[i[1]])
@@ -101,7 +104,7 @@ class ScatterMap3D(GUIWidget):
 
         color = mcolors.to_rgba('Crimson')
         lc = Line3DCollection(
-            candidate_pairs_3d, linewidth=0.5, linestyles='solid', color=color)
+            candidate_pairs, linewidth=0.5, linestyles='solid', color=color)
         
         ax.scatter3D(xvals, yvals, zvals, alpha=0.5, s=.1)
         ax.add_collection(lc)
@@ -124,6 +127,90 @@ class ScatterMap3D(GUIWidget):
         """
         if event == 'Plot':
             fig, _ = self.scatter_figure()
-            pysimplegui_pyplot_draw(window['figure'].TKCanvas, window['figure_controls'].TKCanvas, fig)
+            pysimplegui_pyplot_draw(
+                window['figure'].TKCanvas, window['figure_controls'].TKCanvas,
+                fig)
+        if event == sg.WIN_CLOSED or event == 'broadcast_exit':
+                window.close()
+
+
+class ScatterMap2D(GUIWidget):
+    """
+    Widget to display a 2d scatter map with 2 components (x,y) and unions
+    between candidates
+    """
+
+    def set_params(self, data_points, loop_candidates, columns, **kwargs):
+        """
+        Args:
+            data_points (DataFrame): data frame with data points
+            loop_candidates (List[Tuple[int,int]]): pairs of candidate indexes
+        """
+        self.data_points = data_points
+        self.loop_candidates = loop_candidates
+        self.columns = columns
+
+    def get_layout(self):
+        layout = [
+            [sg.Canvas(key='figure', size=(400 * 2, 400))],
+            [sg.Canvas(key='figure_controls')],
+            [sg.Button('Plot')]
+        ]
+        return layout
+
+    def get_window(self, layout):
+        """
+        This method should create a new window for the current object.
+        """
+        window = sg.Window('Scatter Map 3D', layout, finalize=True)
+        return window
+
+    def scatter_figure(self):
+        """ Helper function to plot with PyPlot """
+        fig = plt.figure()
+        ax = fig.add_subplot()
+
+        ax.set_xlabel(self.columns[0])
+        xvals = self.data_points[self.columns[0]]
+
+        ax.set_ylabel(self.columns[1])
+        yvals = self.data_points[self.columns[1]]
+
+        # given the pairs of candidates as (i1,i2), get the pairs of loops as
+        # ((x1,y1,z1),(x2,y2,z2))
+        candidate_pairs = list(map(
+            lambda i: [
+                    (xvals.iloc[i[0]], yvals.iloc[i[0]]),
+                    (xvals.iloc[i[1]], yvals.iloc[i[1]])
+                ], self.loop_candidates
+        ))
+
+        color = mcolors.to_rgba('Crimson')
+        lc = LineCollection(
+            candidate_pairs, linewidth=0.5, linestyles='solid', color=color)
+        
+        ax.scatter(xvals, yvals, alpha=1, s=10)
+        ax.add_collection(lc)
+        ax.autoscale_view()
+        ax.margins(0.1)
+
+        # Turn off tick labels
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+
+        return fig, ax
+
+    def step(self, window, event, values):
+        """
+        This method receives an event and values for the window it's displaying
+        and the window object.
+        Presumably it operates on the window by inspecting the event and values.
+        It can safely assume this is the window for this widget.
+        """
+        if event == 'Plot':
+            fig, _ = self.scatter_figure()
+            pysimplegui_pyplot_draw(
+                window['figure'].TKCanvas, window['figure_controls'].TKCanvas,
+                fig)
         if event == sg.WIN_CLOSED or event == 'broadcast_exit':
                 window.close()
