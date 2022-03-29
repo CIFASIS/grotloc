18c88a9414424e1afd4979450dec2eb6368e8594
Nicolas Soncini <soncininicolas@gmail.com>
2022-03-29 18:09:04 -0300

    Add simple map widget to display tools

--- grotloc/utils/pysimplegui_utils/loop_map_widget.py
+++ grotloc/utils/pysimplegui_utils/loop_map_widget.py
@@ -18,28 +18,56 @@ __license__ = "GPLv3"
 __version__ = "0.0.1"
 
 from grotloc.utils.pysimplegui_utils.gui_widget import GUIWidget
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
 import PySimpleGUI as sg
+import matplotlib.colors as mcolors
+
+
+def pysimplegui_pyplot_draw(fig_canvas, tool_canvas, fig):
+    """
+    Helper function to draw a PyPlot figure with it's toolbar in PySimpleGui
+
+    Args:
+        fig_canvas (?): pysimplegui canvas to draw the figure
+        tool_canvas (?): pysimplegui canvas to draw the figure
+        fig (?): pyplot figure to draw in the canvas
+    """
+    if fig_canvas.children:
+        for child in fig_canvas.winfo_children():
+            child.destroy()
+    if tool_canvas.children:
+        for child in tool_canvas.winfo_children():
+            child.destroy()
+    figure_canvas_agg = FigureCanvasTkAgg(fig, master=fig_canvas)
+    figure_canvas_agg.draw()
+    toolbar = NavigationToolbar2Tk(figure_canvas_agg, tool_canvas)
+    toolbar.update()
+    figure_canvas_agg.get_tk_widget().pack(side='right', fill='both', expand=1)
+
 
 class ScatterMap3D(GUIWidget):
     """
     Widget to display a 3d scatter map with 3 components (x,y,z) and unions
     between candidates
-
-    TODO: REPLACE ALL
     """
 
-    def set_params(self, path_pairs, **kwargs):
+    def set_params(self, data_points, loop_candidates, columns, **kwargs):
         """
         Args:
-            path_pairs (Tuple[str,str]): pairs of paths to candidate images
+            data_points (DataFrame): data frame with data points
+            loop_candidates (List[Tuple[int,int]]): pairs of candidate indexes
         """
-        self.path_pairs = iter(path_pairs)
-        self.current_paths = next(self.path_pairs)
+        self.data_points = data_points
+        self.loop_candidates = loop_candidates
+        self.columns = columns
 
     def get_layout(self):
         layout = [
-            [sg.Image(self.current_paths[0], key='im1_path')],
-            [sg.Image(self.current_paths[1], key='im2_path')]
+            [sg.Canvas(key='figure', size=(400 * 2, 400))],
+            [sg.Canvas(key='figure_controls')],
+            [sg.Button('Plot')]
         ]
         return layout
 
@@ -47,8 +75,45 @@ class ScatterMap3D(GUIWidget):
         """
         This method should create a new window for the current object.
         """
-        window = sg.Window('Image Visualization', layout, finalize=True)
+        window = sg.Window('Scatter Map 3D', layout, finalize=True)
         return window
+
+    def scatter_figure(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.set_xlabel(self.columns[0])
+        xvals = self.data_points[self.columns[0]]
+
+        ax.set_ylabel(self.columns[1])
+        yvals = self.data_points[self.columns[1]]
+
+        ax.set_zlabel(self.columns[2])
+        zvals = self.data_points[self.columns[2]]
+
+        # given the pairs of candidates as (i1,i2), get the pairs of loops as
+        # ((x1,y1,z1),(x2,y2,z2))
+        candidate_pairs_3d = list(map(
+            lambda i: [
+                    (xvals.iloc[i[0]], yvals.iloc[i[0]], zvals.iloc[i[0]]),
+                    (xvals.iloc[i[1]], yvals.iloc[i[1]], zvals.iloc[i[1]])
+                ], self.loop_candidates
+        ))
+
+        color = mcolors.to_rgba('Crimson')
+        lc = Line3DCollection(
+            candidate_pairs_3d, linewidth=0.5, linestyles='solid', color=color)
+        
+        ax.scatter3D(xvals, yvals, zvals, alpha=0.5, s=.1)
+        ax.add_collection(lc)
+        ax.autoscale_view()
+        ax.margins(0.1)
+
+        # Turn off tick labels
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_zticklabels([])
+
+        return fig, ax
 
     def step(self, window, event, values):
         """
@@ -57,12 +122,8 @@ class ScatterMap3D(GUIWidget):
         Presumably it operates on the window by inspecting the event and values.
         It can safely assume this is the window for this widget.
         """
-        if event == 'broadcast_next':
-            try:
-                self.current_paths = next(self.path_pairs)
-                window['im1_path'].update(self.current_paths[0])
-                window['im2_path'].update(self.current_paths[1])
-            except StopIteration:
-                window.close()
+        if event == 'Plot':
+            fig, _ = self.scatter_figure()
+            pysimplegui_pyplot_draw(window['figure'].TKCanvas, window['figure_controls'].TKCanvas, fig)
         if event == sg.WIN_CLOSED or event == 'broadcast_exit':
                 window.close()
